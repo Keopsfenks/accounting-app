4be508f167551ff402fafa8f8a73240079cc2d55
Semih Gürbüz <sgurbuzofficial@gmail.com>
2024-12-13 09:47:01 +0300

    User and abstract entities is added. Create context and DependencyInjection method is added this method configuration Persistance layer

---
+++ Persistance/Contexts/ApplicationDb/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance.Contexts.ApplicationDb;
+
+internal sealed class AppDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid> {
+	public AppDbContext(DbContextOptions options) : base(options) { }
+	
+	protected override void OnModelCreating(ModelBuilder builder) {
+		builder.ApplyConfigurationsFromAssembly(typeof(DependencyInjection).Assembly);
+
+		builder.Ignore<IdentityUserLogin<Guid>>();
+		builder.Ignore<IdentityRoleClaim<Guid>>();
+		builder.Ignore<IdentityUserToken<Guid>>();
+		builder.Ignore<IdentityUserRole<Guid>>();
+		builder.Ignore<IdentityUserClaim<Guid>>();
+	}
+}
---
+++ Persistance/DependencyInjection.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistance.Contexts.ApplicationDb;
+using Scrutor;
+
+namespace Persistance;
+
+public static class DependencyInjection {
+	public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration) {
+		services.AddDbContext<AppDbContext>(options => {
+			options.UseSqlServer(configuration.GetConnectionString("SqlServer"));
+		});
+
+		services
+			.AddIdentityCore<AppUser>(cfr => {
+				cfr.Password.RequiredLength         = 1;
+				cfr.Password.RequireNonAlphanumeric = false;
+				cfr.Password.RequireUppercase       = false;
+				cfr.Password.RequireLowercase       = false;
+				cfr.Password.RequireDigit           = false;
+				cfr.SignIn.RequireConfirmedEmail    = true;
+				cfr.Lockout.DefaultLockoutTimeSpan  = TimeSpan.FromMinutes(5);
+				cfr.Lockout.MaxFailedAccessAttempts = 3;
+				cfr.Lockout.AllowedForNewUsers      = true;
+			})
+			.AddEntityFrameworkStores<AppDbContext>();
+
+
+		services.Scan(action => {
+			action
+				.FromAssemblies(Assembly.GetExecutingAssembly())
+				.AddClasses(publicOnly: false)
+				.UsingRegistrationStrategy(RegistrationStrategy.Skip)
+				.AsMatchingInterface()
+				.AsImplementedInterfaces()
+				.WithScopedLifetime();
+		});
+
+		return services;
+	}
+	
+}
